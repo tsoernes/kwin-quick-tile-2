5849fd51a5c686f83aa5fa21ffb234c7efa089e8
torstein <torstein@localhost.localdomain>
2017-07-14 15:11:23 +0200

    Initial

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,30 +1,27 @@
 /*******************************************************************************
- * Quick Tiling Extensions
+ * Quick Tile 2 KWin script
  *
- * A Script for KWin - the KDE window manager
+ * This script will add four additional shortcuts to the KWin window manager,
+ * in order to emulate Windows 10 tiling behavior.
  *
- * This script will add two additional shortcuts to the KWin window manager,
- * they will change the Win+Up and Win+Down behavior similar to the 2x2 snap
- * feature newly introduced in Windows 10.
- *
- * Copyright (C) 2016 Koen Hausmans <koen@hausmans.nl>
+ * Original author Koen Hausmans <koen@hausmans.nl>
  ******************************************************************************/
 
-function _GetCurrentScreenGeometry() {
-    return workspace.clientArea(KWin.PlacementArea, workspace.activeScreen, workspace.currentDesktop);
+function _GetScreenGeometry() {
+    return workspace.clientArea(KWin.PlacementArea, workspace.activeScreen, workspace.Desktop);
 }
 
-function _GetClientGeometryOnCurrentScreen() {
+function _GetClientGeometryOnScreen() {
     var clientGeometry = workspace.activeClient.geometry;
-    var screenGeometry = _GetCurrentScreenGeometry();
+    var screenGeometry = _GetScreenGeometry();
     clientGeometry.x -= screenGeometry.x;
     clientGeometry.y -= screenGeometry.y;
     return clientGeometry;
 }
 
-function _IsCurrentWindowVerticallyMaximized() {
-    var screenGeometry = _GetCurrentScreenGeometry();
-    var clientGeometry = _GetClientGeometryOnCurrentScreen();
+function _IsWindowVerticallyMaximized() {
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
     if (clientGeometry.height == screenGeometry.height) {
         return true;
     }
@@ -32,9 +29,23 @@ function _IsCurrentWindowVerticallyMaximized() {
     return false
 }
 
+function _IsWindowHorizontallyMaximized() {
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
+    if (clientGeometry.width == screenGeometry.width) {
+        return true;
+    }
+
+    return false
+}
+
+function _IsWindowMaximized() {
+    return (_IsWindowHorizontallyMaximized() && _IsWindowVerticallyMaximized());
+}
+
 function _IsWindowTiledToTop() {
-    var screenGeometry = _GetCurrentScreenGeometry();
-    var clientGeometry = _GetClientGeometryOnCurrentScreen();
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
     if (clientGeometry.height == (screenGeometry.height / 2) && clientGeometry.y == 0) {
         return true;
     }
@@ -43,8 +54,8 @@ function _IsWindowTiledToTop() {
 }
 
 function _IsWindowTiledToBottom() {
-    var screenGeometry = _GetCurrentScreenGeometry();
-    var clientGeometry = _GetClientGeometryOnCurrentScreen();
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
     if (clientGeometry.height == (screenGeometry.height / 2) && clientGeometry.y == (screenGeometry.height / 2)) {
         return true;
     }
@@ -53,8 +64,8 @@ function _IsWindowTiledToBottom() {
 }
 
 function _IsWindowTiledToLeft() {
-    var screenGeometry = _GetCurrentScreenGeometry();
-    var clientGeometry = _GetClientGeometryOnCurrentScreen();
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
     if (clientGeometry.width == (screenGeometry.width / 2) && clientGeometry.x == 0) {
         return true;
     }
@@ -63,8 +74,8 @@ function _IsWindowTiledToLeft() {
 }
 
 function _IsWindowTiledToRight() {
-    var screenGeometry = _GetCurrentScreenGeometry();
-    var clientGeometry = _GetClientGeometryOnCurrentScreen();
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
     if (clientGeometry.width == (screenGeometry.width / 2) && clientGeometry.x == (screenGeometry.width / 2)) {
         return true;
     }
@@ -72,11 +83,21 @@ function _IsWindowTiledToRight() {
     return false;
 }
 
-var QuickTileUpwards = function() {
+function _IsWindowTiledToQuadrant() {
+    var screenGeometry = _GetScreenGeometry();
+    var clientGeometry = _GetClientGeometryOnScreen();
+    if (clientGeometry.width == (screenGeometry.width / 2) && clientGeometry.height == (screenGeometry.height/ 2)) {
+        return true;
+    }
+
+    return false;
+}
+
+var QuickTileUp = function() {
     if (_IsWindowTiledToLeft()) {
         if (_IsWindowTiledToTop()) {
             workspace.slotWindowMaximize();
-        } else if (_IsCurrentWindowVerticallyMaximized()) {
+        } else if (_IsWindowVerticallyMaximized()) {
             workspace.slotWindowQuickTileTopLeft();
         } else if (_IsWindowTiledToBottom()) {
             workspace.slotWindowQuickTileLeft();
@@ -84,7 +105,7 @@ var QuickTileUpwards = function() {
     } else if (_IsWindowTiledToRight()) {
         if (_IsWindowTiledToTop()) {
             workspace.slotWindowMaximize();
-        } else if (_IsCurrentWindowVerticallyMaximized()) {
+        } else if (_IsWindowVerticallyMaximized()) {
             workspace.slotWindowQuickTileTopRight();
         } else if (_IsWindowTiledToBottom()) {
             workspace.slotWindowQuickTileRight();
@@ -94,11 +115,11 @@ var QuickTileUpwards = function() {
     }
 }
 
-var QuickTileDownwards = function() {
+var QuickTileDown = function() {
     if (_IsWindowTiledToLeft()) {
         if (_IsWindowTiledToTop()) {
             workspace.slotWindowQuickTileLeft();
-        } else if (_IsCurrentWindowVerticallyMaximized()) {
+        } else if (_IsWindowVerticallyMaximized()) {
             workspace.slotWindowQuickTileBottomLeft();
         } else if (_IsWindowTiledToBottom()) {
             workspace.slotWindowMinimize();
@@ -106,7 +127,7 @@ var QuickTileDownwards = function() {
     } else if (_IsWindowTiledToRight()) {
         if (_IsWindowTiledToTop()) {
             workspace.slotWindowQuickTileRight();
-        } else if (_IsCurrentWindowVerticallyMaximized()) {
+        } else if (_IsWindowVerticallyMaximized()) {
             workspace.slotWindowQuickTileBottomRight();
         } else if (_IsWindowTiledToBottom()) {
             workspace.slotWindowMinimize();
@@ -116,7 +137,38 @@ var QuickTileDownwards = function() {
     }
 }
 
-var shortcutPrefix = "Quick Tiling - Windows 10: ";
-registerShortcut(shortcutPrefix + "Upwards", shortcutPrefix + "Upwards", "Meta+Up", QuickTileUpwards);
-registerShortcut(shortcutPrefix + "Downwards", shortcutPrefix + "Downwards", "Meta+Down", QuickTileDownwards);
+var QuickTileLeft = function() {
+    if (_IsWindowTiledToQuadrant() && _IsWindowTiledToRight()) {
+	workspace.slotWindowMaximizeHorizontal();
+    } else if (_IsWindowTiledToTop()) {
+	workspace.slotWindowQuickTileTopLeft();
+    } else if (_IsWindowTiledToBottom()) {
+	workspace.slotWindowQuickTileBottomLeft();
+    } else if (_IsWindowMaximized()) {
+	workspace.slotWindowQuickTileLeft();
+	workspace.slotWindowQuickTileLeft();
+    } else {
+        workspace.slotWindowQuickTileLeft();
+    }
+}
 
+var QuickTileRight = function() {
+    if (_IsWindowTiledToQuadrant() && _IsWindowTiledToLeft()) {
+	workspace.slotWindowMaximizeHorizontal();
+    } else if (_IsWindowTiledToTop()) {
+	workspace.slotWindowQuickTileTopRight();
+    } else if (_IsWindowTiledToBottom()) {
+	workspace.slotWindowQuickTileBottomRight();
+    } else if (_IsWindowMaximized()) {
+	workspace.slotWindowQuickTileRight();
+	workspace.slotWindowQuickTileRight();
+    } else {
+        workspace.slotWindowQuickTileRight();
+    }
+}
+
+var shortcutPrefix = "Quick Tile 2 ";
+registerShortcut(shortcutPrefix + "Up", shortcutPrefix + "Up", "Meta+Up", QuickTileUp);
+registerShortcut(shortcutPrefix + "Down", shortcutPrefix + "Down", "Meta+Down", QuickTileDown);
+registerShortcut(shortcutPrefix + "Left", shortcutPrefix + "Left", "Meta+Left", QuickTileLeft);
+registerShortcut(shortcutPrefix + "Right", shortcutPrefix + "Right", "Meta+Right", QuickTileRight);
