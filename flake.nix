b3dfbf04ef3b617e8b7f6431cf3ccb4a50eff1ce
Tim Hilt <timhilt@live.de>
2021-05-31 17:59:39 +0200

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell { buildInputs = with pkgs; [ qt5.qttools zip ]}
+  }
+}
