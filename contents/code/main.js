7487840f122555b29f7b2409669eed82da261194
Tim Hilt <thilt@in-technology.eu>
2022-04-20 18:03:12 +0200

    master: Making it work for multi-monitor-setups

--- contents/code/main.js
+++ contents/code/main.js
@@ -10,212 +10,256 @@
  ******************************************************************************/
 
 function _GetScreenGeometry() {
-    return workspace.clientArea(KWin.PlacementArea, workspace.activeScreen, workspace.Desktop)
+  const screenGeometry = workspace.clientArea(
+    KWin.PlacementArea,
+    workspace.activeClient.screen,
+    workspace.Desktop
+  );
+  return screenGeometry;
 }
 
 function _GetClientGeometryOnScreen() {
-    const clientGeometry = workspace.activeClient.geometry
-    const screenGeometry = _GetScreenGeometry()
-    const x = clientGeometry.x - screenGeometry.x
-    const y = clientGeometry.y - screenGeometry.y
-    return {x: x, y: y, width: clientGeometry.width, height: clientGeometry.height}
+  const clientGeometry = workspace.activeClient.geometry;
+  const screenGeometry = _GetScreenGeometry();
+  const x = clientGeometry.x - screenGeometry.x;
+  const y = clientGeometry.y - screenGeometry.y;
+  return {
+    x: x,
+    y: y,
+    width: clientGeometry.width,
+    height: clientGeometry.height,
+  };
 }
 
 function _IsVerticallyMaximized() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.height === screenGeometry.height) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (clientGeometry.height === screenGeometry.height) {
+    return true;
+  }
+  return false;
 }
 
 function _IsHorizontallyMaximized() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.width === screenGeometry.width) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (clientGeometry.width === screenGeometry.width) {
+    return true;
+  }
+  return false;
 }
 
 function _IsMaximized() {
-    return _IsHorizontallyMaximized() && _IsVerticallyMaximized()
+  return _IsHorizontallyMaximized() && _IsVerticallyMaximized();
 }
 
 function _IsTiledToTop() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.height === (screenGeometry.height / 2) && clientGeometry.y === 0) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (
+    clientGeometry.height === screenGeometry.height / 2 &&
+    clientGeometry.y === 0
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function _IsTiledTop() {
-    return _IsTiledToTop() && _IsHorizontallyMaximized()
+  return _IsTiledToTop() && _IsHorizontallyMaximized();
 }
 
 function _IsTiledToBottom() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.height === (screenGeometry.height / 2) && clientGeometry.y === (screenGeometry.height / 2)) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (
+    clientGeometry.height === screenGeometry.height / 2 &&
+    clientGeometry.y === screenGeometry.height / 2
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function _IsTiledBottom() {
-    return _IsTiledToBottom() && _IsHorizontallyMaximized()
+  return _IsTiledToBottom() && _IsHorizontallyMaximized();
 }
 
 function _IsTiledToLeft() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.width === (screenGeometry.width / 2) && clientGeometry.x === 0) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (
+    clientGeometry.width === screenGeometry.width / 2 &&
+    clientGeometry.x === 0
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function _IsTiledLeft() {
-    return _IsTiledToLeft() && _IsVerticallyMaximized()
+  return _IsTiledToLeft() && _IsVerticallyMaximized();
 }
 
 function _IsTiledToRight() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.width === (screenGeometry.width / 2) && clientGeometry.x === (screenGeometry.width / 2)) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (
+    clientGeometry.width === screenGeometry.width / 2 &&
+    clientGeometry.x === screenGeometry.width / 2
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function _IsTiledRight() {
-    return _IsTiledToRight() && _IsVerticallyMaximized()
+  return _IsTiledToRight() && _IsVerticallyMaximized();
 }
 
 function _IsTiledToQuadrant() {
-    const screenGeometry = _GetScreenGeometry()
-    const clientGeometry = _GetClientGeometryOnScreen()
-    if (clientGeometry.width === (screenGeometry.width / 2) && clientGeometry.height === (screenGeometry.height/ 2)) {
-        return true
-    }
-    return false
+  const screenGeometry = _GetScreenGeometry();
+  const clientGeometry = _GetClientGeometryOnScreen();
+  if (
+    clientGeometry.width === screenGeometry.width / 2 &&
+    clientGeometry.height === screenGeometry.height / 2
+  ) {
+    return true;
+  }
+  return false;
 }
 
 function _IsTiledTopLeft() {
-    return _IsTiledToTop() && _IsTiledToLeft()
+  return _IsTiledToTop() && _IsTiledToLeft();
 }
 
 function _IsTiledTopRight() {
-    return _IsTiledToTop() && _IsTiledToRight()
+  return _IsTiledToTop() && _IsTiledToRight();
 }
 
 function _IsTiledBottomLeft() {
-    return _IsTiledToBottom() && _IsTiledToLeft()
+  return _IsTiledToBottom() && _IsTiledToLeft();
 }
 
 function _IsTiledBottomRight() {
-    return _IsTiledToBottom() && _IsTiledToRight()
+  return _IsTiledToBottom() && _IsTiledToRight();
 }
 
-var QuickTileUp = function() {
-    // L > TL
-    if (_IsTiledLeft()) {
-        workspace.slotWindowQuickTileTopLeft()
+var QuickTileUp = function () {
+  // L > TL
+  if (_IsTiledLeft()) {
+    workspace.slotWindowQuickTileTopLeft();
     // R > TR
-    } else if (_IsTiledRight()) {
-        workspace.slotWindowQuickTileTopRight()
+  } else if (_IsTiledRight()) {
+    workspace.slotWindowQuickTileTopRight();
     // B > T
-    } else if (_IsTiledBottom()) {
-        workspace.slotWindowQuickTileTop()
+  } else if (_IsTiledBottom()) {
+    workspace.slotWindowQuickTileTop();
     // BL > L
-    } else if (_IsTiledBottomLeft()) {
-        workspace.slotWindowQuickTileLeft()
+  } else if (_IsTiledBottomLeft()) {
+    workspace.slotWindowQuickTileLeft();
     // BR > R
-    } else if (_IsTiledBottomRight()) {
-        workspace.slotWindowQuickTileRight()
+  } else if (_IsTiledBottomRight()) {
+    workspace.slotWindowQuickTileRight();
     // M > T
     // this is probaly no good for multi-monitor
-    } else if (_IsMaximized()) {
-        workspace.slotWindowMaximize()
-    } else {
-        workspace.slotWindowMaximize()
-    }
-}
+  } else if (_IsMaximized()) {
+    workspace.slotWindowMaximize();
+  } else {
+    workspace.slotWindowMaximize();
+  }
+};
 
-var QuickTileDown = function() {
-    // L > BL
-    if (_IsTiledLeft()) {
-        workspace.slotWindowQuickTileBottomLeft()
+var QuickTileDown = function () {
+  // L > BL
+  if (_IsTiledLeft()) {
+    workspace.slotWindowQuickTileBottomLeft();
     // R > BR
-    } else if (_IsTiledRight()) {
-        workspace.slotWindowQuickTileBottomRight()
+  } else if (_IsTiledRight()) {
+    workspace.slotWindowQuickTileBottomRight();
     // T > B
-    } else if (_IsTiledTop()) {
-      workspace.slotWindowQuickTileBottom()
+  } else if (_IsTiledTop()) {
+    workspace.slotWindowQuickTileBottom();
     // TL > L
-    } else if (_IsTiledTopLeft()) {
-        workspace.slotWindowQuickTileLeft()
+  } else if (_IsTiledTopLeft()) {
+    workspace.slotWindowQuickTileLeft();
     // TR > R
-    } else if (_IsTiledTopRight()) {
-        workspace.slotWindowQuickTileRight()
+  } else if (_IsTiledTopRight()) {
+    workspace.slotWindowQuickTileRight();
     // M > B
-    } else if (_IsMaximized()) {
-        workspace.slotWindowMaximize()
-    } else {
-        workspace.slotWindowMinimize()
-    }
-}
+  } else if (_IsMaximized()) {
+    workspace.slotWindowMaximize();
+  } else {
+    workspace.slotWindowMinimize();
+  }
+};
 
-var QuickTileLeft = function() {
-    // T > TL
-    if (_IsTiledTop()) {
-        workspace.slotWindowQuickTileTopLeft()
+var QuickTileLeft = function () {
+  // T > TL
+  if (_IsTiledTop()) {
+    workspace.slotWindowQuickTileTopLeft();
     // B > BL
-    } else if (_IsTiledBottom()) {
-        workspace.slotWindowQuickTileBottomLeft()
+  } else if (_IsTiledBottom()) {
+    workspace.slotWindowQuickTileBottomLeft();
     // TR > T
-    } else if (_IsTiledTopRight()) {
-        workspace.slotWindowQuickTileTop()
+  } else if (_IsTiledTopRight()) {
+    workspace.slotWindowQuickTileTop();
     // BR > B
-    } else if (_IsTiledBottomRight()) {
-        workspace.slotWindowQuickTileBottom()
+  } else if (_IsTiledBottomRight()) {
+    workspace.slotWindowQuickTileBottom();
     // M > L
-    } else if (_IsMaximized()) {
-        workspace.slotWindowQuickTileLeft()
+  } else if (_IsMaximized()) {
+    workspace.slotWindowQuickTileLeft();
     // R > L, BL > L, TL > L
-    } else {
-        workspace.slotWindowQuickTileLeft()
-    }
-}
+  } else {
+    workspace.slotWindowQuickTileLeft();
+  }
+};
 
-var QuickTileRight = function() {
-    // T > TR
-    if (_IsTiledTop()) {
-        workspace.slotWindowQuickTileTopRight()
+var QuickTileRight = function () {
+  // T > TR
+  if (_IsTiledTop()) {
+    workspace.slotWindowQuickTileTopRight();
     // B > BR
-    } else if (_IsTiledBottom()) {
-        workspace.slotWindowQuickTileBottomRight()
+  } else if (_IsTiledBottom()) {
+    workspace.slotWindowQuickTileBottomRight();
     // TL > T
-    } else if (_IsTiledTopLeft()) {
-        workspace.slotWindowQuickTileTop()
+  } else if (_IsTiledTopLeft()) {
+    workspace.slotWindowQuickTileTop();
     // BL > B
-    } else if (_IsTiledBottomLeft()) {
-        workspace.slotWindowQuickTileBottom()
+  } else if (_IsTiledBottomLeft()) {
+    workspace.slotWindowQuickTileBottom();
     // M > R
-    } else if (_IsMaximized()) {
-        workspace.slotWindowQuickTileRight()
+  } else if (_IsMaximized()) {
+    workspace.slotWindowQuickTileRight();
     // L > R, BR > R, TR > R
-    } else {
-        workspace.slotWindowQuickTileRight()
-    }
-}
+  } else {
+    workspace.slotWindowQuickTileRight();
+  }
+};
 
-
-var shortcutPrefix = "Quick Tile 2 "
-registerShortcut(shortcutPrefix + "Up", shortcutPrefix + "Up", "Meta+Up", QuickTileUp)
-registerShortcut(shortcutPrefix + "Down", shortcutPrefix + "Down", "Meta+Down", QuickTileDown)
-registerShortcut(shortcutPrefix + "Left", shortcutPrefix + "Left", "Meta+Left", QuickTileLeft)
-registerShortcut(shortcutPrefix + "Right", shortcutPrefix + "Right", "Meta+Right", QuickTileRight)
+var shortcutPrefix = "Quick Tile 2 ";
+registerShortcut(
+  shortcutPrefix + "Up",
+  shortcutPrefix + "Up",
+  "Meta+Up",
+  QuickTileUp
+);
+registerShortcut(
+  shortcutPrefix + "Down",
+  shortcutPrefix + "Down",
+  "Meta+Down",
+  QuickTileDown
+);
+registerShortcut(
+  shortcutPrefix + "Left",
+  shortcutPrefix + "Left",
+  "Meta+Left",
+  QuickTileLeft
+);
+registerShortcut(
+  shortcutPrefix + "Right",
+  shortcutPrefix + "Right",
+  "Meta+Right",
+  QuickTileRight
+);
--- flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-
-  outputs = { self, nixpkgs }:
-  let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShell.x86_64-linux = pkgs.mkShell { buildInputs = with pkgs; [ qt5.qttools zip ]; };
-  };
-}
