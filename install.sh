8c8b9ccdffd9f6e8870e1b5fc8cbef32649c49ce
Andreas Schneider <aksdb@gmx.de>
2024-02-20 15:09:00 +0100

    👷 Update install script

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-rm ./quick-tile-*
-zip -r quick-tile-2-v$(cat metadata.desktop | grep X-KDE-PluginInfo-Version= | awk -F'=' '{print $2}').kwinscript *
-rm -rf ~/.local/share/kwin/scripts/quick-tile-2*
-plasmapkg2 --type kwinscript -i quick-tile-2-v*.kwinscript
-qdbus org.kde.KWin /KWin reconfigure
-mkdir -p ~/.local/share/kservices5
-cp ~/.local/share/kwin/scripts/quick-tile-2/metadata.desktop ~/.local/share/kservices5/kwin-script-quick-tile-2.desktop
-kwin_x11 --replace >/dev/null 2>&1 &
+kpackagetool6 --type=KWin/Script -i .
